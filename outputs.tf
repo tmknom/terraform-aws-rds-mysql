1b7400798f5ad07ef37b42ccfa0a04620f6a5614
tmknom <tmk.nom@gmail.com>
2018-12-12 00:07:47 +0900

    Implement DB Instance

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,104 @@
+output "db_instance_address" {
+  value       = "${aws_db_instance.default.address}"
+  description = "The hostname of the RDS instance. See also endpoint and port."
+}
 
+output "db_instance_arn" {
+  value       = "${aws_db_instance.default.arn}"
+  description = "The ARN of the RDS instance."
+}
+
+output "db_instance_allocated_storage" {
+  value       = "${aws_db_instance.default.allocated_storage}"
+  description = "The amount of allocated storage."
+}
+
+output "db_instance_availability_zone" {
+  value       = "${aws_db_instance.default.availability_zone}"
+  description = "The availability zone of the instance."
+}
+
+output "db_instance_backup_retention_period" {
+  value       = "${aws_db_instance.default.backup_retention_period}"
+  description = "The backup retention period."
+}
+
+output "db_instance_backup_window" {
+  value       = "${aws_db_instance.default.backup_window}"
+  description = "The backup window."
+}
+
+output "db_instance_ca_cert_identifier" {
+  value       = "${aws_db_instance.default.ca_cert_identifier}"
+  description = "Specifies the identifier of the CA certificate for the DB instance."
+}
+
+output "db_instance_endpoint" {
+  value       = "${aws_db_instance.default.endpoint}"
+  description = ".The connection endpoint in address:port format."
+}
+
+output "db_instance_engine" {
+  value       = "${aws_db_instance.default.engine}"
+  description = "The database engine."
+}
+
+output "db_instance_engine_version" {
+  value       = "${aws_db_instance.default.engine_version}"
+  description = "The database engine version."
+}
+
+output "db_instance_hosted_zone_id" {
+  value       = "${aws_db_instance.default.hosted_zone_id}"
+  description = "The canonical hosted zone ID of the DB instance (to be used in a Route 53 Alias record)."
+}
+
+output "db_instance_id" {
+  value       = "${aws_db_instance.default.id}"
+  description = "The RDS instance ID."
+}
+
+output "db_instance_instance_class" {
+  value       = "${aws_db_instance.default.instance_class}"
+  description = "The RDS instance class."
+}
+
+output "db_instance_maintenance_window" {
+  value       = "${aws_db_instance.default.maintenance_window}"
+  description = "The instance maintenance window."
+}
+
+output "db_instance_multi_az" {
+  value       = "${aws_db_instance.default.multi_az}"
+  description = "If the RDS instance is multi AZ enabled."
+}
+
+output "db_instance_name" {
+  value       = "${aws_db_instance.default.name}"
+  description = "The database name."
+}
+
+output "db_instance_port" {
+  value       = "${aws_db_instance.default.port}"
+  description = "The database port."
+}
+
+output "db_instance_resource_id" {
+  value       = "${aws_db_instance.default.resource_id}"
+  description = "The RDS Resource ID of this instance."
+}
+
+output "db_instance_status" {
+  value       = "${aws_db_instance.default.status}"
+  description = "The RDS instance status."
+}
+
+output "db_instance_storage_encrypted" {
+  value       = "${aws_db_instance.default.storage_encrypted}"
+  description = "Specifies whether the DB instance is encrypted."
+}
+
+output "db_instance_username" {
+  value       = "${aws_db_instance.default.username}"
+  description = "The master username for the database."
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,170 @@
+variable "identifier" {
+  type        = "string"
+  description = " The name of the RDS instance."
+}
 
+variable "engine_version" {
+  type        = "string"
+  description = "The engine version to use."
+}
+
+variable "instance_class" {
+  type        = "string"
+  description = "The instance type of the RDS instance."
+}
+
+variable "allocated_storage" {
+  type        = "string"
+  description = "The allocated storage in gibibytes."
+}
+
+variable "name" {
+  type        = "string"
+  description = "The name of the database to create when the DB instance is created."
+}
+
+variable "username" {
+  type        = "string"
+  description = "Username for the master DB user."
+}
+
+variable "password" {
+  type        = "string"
+  description = "Password for the master DB user."
+}
+
+variable "maintenance_window" {
+  type        = "string"
+  description = "The window to perform maintenance in."
+}
+
+variable "backup_window" {
+  type        = "string"
+  description = "The daily time range (in UTC) during which automated backups are created if they are enabled."
+}
+
+variable "apply_immediately" {
+  default     = false
+  type        = "string"
+  description = "Specifies whether any database modifications are applied immediately, or during the next maintenance window."
+}
+
+variable "multi_az" {
+  default     = true
+  type        = "string"
+  description = "Specifies if the RDS instance is multi-AZ."
+}
+
+variable "port" {
+  default     = 3306
+  type        = "string"
+  description = "The port on which the DB accepts connections."
+}
+
+variable "storage_type" {
+  default     = "gp2"
+  type        = "string"
+  description = "One of standard (magnetic), gp2 (general purpose SSD), or io1 (provisioned IOPS SSD)."
+}
+
+variable "iops" {
+  default     = 0
+  type        = "string"
+  description = "The amount of provisioned IOPS. Setting this implies a storage_type of io1."
+}
+
+variable "auto_minor_version_upgrade" {
+  default     = true
+  type        = "string"
+  description = "Indicates that minor engine upgrades will be applied automatically to the DB instance during the maintenance window.."
+}
+
+variable "allow_major_version_upgrade" {
+  default     = true
+  type        = "string"
+  description = "Indicates that major version upgrades are allowed."
+}
+
+variable "backup_retention_period" {
+  default     = "30"
+  type        = "string"
+  description = "The days to retain backups for. Must be between 0 and 35."
+}
+
+variable "storage_encrypted" {
+  default     = true
+  type        = "string"
+  description = "Specifies whether the DB instance is encrypted."
+}
+
+variable "kms_key_id" {
+  default     = ""
+  type        = "string"
+  description = "The ARN for the KMS encryption key."
+}
+
+variable "deletion_protection" {
+  default     = true
+  type        = "string"
+  description = "If the DB instance should have deletion protection enabled."
+}
+
+variable "final_snapshot_identifier" {
+  default     = "final-snapshot"
+  type        = "string"
+  description = "The name of your final DB snapshot when this DB instance is deleted."
+}
+
+variable "skip_final_snapshot" {
+  default     = false
+  type        = "string"
+  description = "Determines whether a final DB snapshot is created before the DB instance is deleted."
+}
+
+variable "enabled_cloudwatch_logs_exports" {
+  default     = []
+  type        = "list"
+  description = "List of log types to enable for exporting to CloudWatch logs."
+}
+
+variable "monitoring_interval" {
+  default     = 0
+  type        = "string"
+  description = "The interval, in seconds, between points when Enhanced Monitoring metrics are collected for the DB instance."
+}
+
+variable "monitoring_role_arn" {
+  default     = ""
+  type        = "string"
+  description = "The ARN for the IAM role that permits RDS to send enhanced monitoring metrics to CloudWatch Logs."
+}
+
+variable "iam_database_authentication_enabled" {
+  default     = true
+  type        = "string"
+  description = "Specifies whether or mappings of IAM accounts to database accounts is enabled."
+}
+
+variable "copy_tags_to_snapshot" {
+  default     = true
+  type        = "string"
+  description = "On delete, copy all Instance tags to the final snapshot."
+}
+
+variable "publicly_accessible" {
+  default     = false
+  type        = "string"
+  description = "Bool to control if instance is publicly accessible."
+}
+
+variable "license_model" {
+  default     = "general-public-license"
+  type        = "string"
+  description = "License model information for this DB instance."
+}
+
+variable "tags" {
+  default     = {}
+  type        = "map"
+  description = "A mapping of tags to assign to all resources."
+}
