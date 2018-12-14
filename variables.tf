21d2923bac759454c868d15f32e76b341e9cfe07
tmknom <tmk.nom@gmail.com>
2018-12-14 13:13:54 +0900

    Add default value to name

--- variables.tf
+++ variables.tf
@@ -19,6 +19,7 @@ variable "allocated_storage" {
 }
 
 variable "name" {
+  default     = ""
   type        = "string"
   description = "The name of the database to create when the DB instance is created."
 }
