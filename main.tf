c6a25c7fb165a6964f0d5391fe3bd36294c29737
tmknom <tmk.nom@gmail.com>
2020-05-01 20:15:40 +0900

    Remove suffix from DB Subnet Group

--- main.tf
+++ main.tf
@@ -325,7 +325,7 @@ locals {
 
 # https://www.terraform.io/docs/providers/aws/r/db_subnet_group.html
 resource "aws_db_subnet_group" "default" {
-  name        = "${var.identifier}-rds-mysql"
+  name        = var.identifier
   subnet_ids  = var.subnet_ids
   description = var.description
 
